7f021851ce5b84a7048f2feeb47d31c6e618e39e
vira-tresyana <71061869+vira-tresyana@users.noreply.github.com>
2020-10-27 07:50:14 +0700

    Initial commit

--- system/resources/views/login.blade.php
+++ system/resources/views/login.blade.php
@@ -1,76 +1,82 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="assets/css/style.css" rel="stylesheet">
-	 <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>virashop - Index</title>
-  <meta content="" name="descriptison">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Restaurantly - v1.1.0
-  * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-	<title>Login LK</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{url('public')}}/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{url('public')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{url('public')}}/dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body id="bg-login">
-	<!-- ======= Top Bar ======= -->
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="{{url('public')}}/index2.html"><b>VIRA</b>SHOP</a>
   </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
-
-      <h1 class="logo mr-auto"><a href="index.html">virashop</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li class="active"><a href={{ url("/index")}}>Home</a></li>
-          <li><a href={{ url("/index")}}>Produk</a></li>
-          <li><a href={{ url("/index")}}>Pesan</a></li>
-          <li><a href={{ url("/login")}}>Login</a></li>
-          
-        </ul>
-      </nav><!-- .nav-menu -->
-
+      <form action="{{url('/admin/beranda')}}">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- /.social-auth-links -->
+      <p class="mb-0">
+        <a href="{{url('/registrasi')}}" class="text-center">Register a new membership</a>
+      </p>
     </div>
-  </header><!-- End Header -->
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
 
-  <!-- ======= Hero Section ======= -->
-	<div class="log-1">
-		<h2>Login</h2>
-		<form action={{ url("/index")}} method="">
-			<input type="text" name="user" placeholder="Username" class="input-control">
-			<input type="password" name="pass" placeholder="Password" class="input-control">
-			<input type="submit" name="masuk" value="Login" class="btn">
-		</form>
-	</div>
+<!-- jQuery -->
+<script src="{{url('public')}}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{url('public')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{url('public')}}/dist/js/adminlte.min.js"></script>
+
 </body>
 </html>
--- system/routes/web.php
+++ system/routes/web.php
@@ -24,3 +24,29 @@ Route::get('/login', function () {
 Route::get('/index', function () {
     return view('index');
 });
+
+Route::get('/admin/beranda', function () {
+    return view('admin/beranda');
+});
+
+Route::get('/admin/kategori', function () {
+    return view('admin/kategori');
+});
+
+Route::get('/admin/produk', function () {
+    return view('admin/produk');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/registrasi', function () {
+    return view('registrasi');
+});
+
+
+Route::get('/user/', function () {
+    return view('user.beranda');
+});
+
